69fccc4a3aba034a5c866d0f1d819f6917f9b9db
iam-gourav <140313751+iam-gourav@users.noreply.github.com>
2024-01-24 11:32:36 +0530

    m

---
+++ password-genrator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className='bg-black text-red-500'>
+      <h1 className='text-blue-400'>hello</h1>
+    </div>
+  )
+}
+
+export default App
