9cd9c378eb42c1c32c706b40151e35b6e37d4702
iam-gourav <140313751+iam-gourav@users.noreply.github.com>
2024-01-29 10:57:27 +0530

    passstyle

--- password-genrator/src/App.jsx
+++ password-genrator/src/App.jsx
@@ -3,12 +3,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+
 
   return (
-    <div className='bg-black text-red-500'>
-      <h1 className='text-blue-400'>hello</h1>
-    </div>
+    <>
+      <div className='w-full h-[100vh] bg-black text-white flex justify-center flex-col items-center gap-10'>
+              <h1 className='text-white text-4xl'>Password-Genrator</h1>
+        <div >
+
+
+        <input type="text" className='w-[250px]'  />
+        <button className='bg-blue-500 border-2 border-white mx-4 w-20'>copy</button>
+        </div>
+        <div className='flex justify-center  items-center gap-4'>
+          <span>5</span>
+          <input type="range" minLength={5} maxLength={100} />
+          <span>100</span>
+          <input type="checkbox" name="" id="" />
+          <label htmlFor="Number">Number</label>
+          <input type="checkbox" name="" id="" />
+          <label htmlFor="">Symbol</label>
+        </div>
+      </div>
+    </>
   )
 }
 
