8736833077a91ef7074034be77f072268f17d959
iam-gourav <140313751+iam-gourav@users.noreply.github.com>
2024-02-01 16:42:54 +0530

    copy

--- password-genrator/src/App.jsx
+++ password-genrator/src/App.jsx
@@ -1,77 +1,88 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 import './App.css'
 
 function App() {
 
-const [length , setLength] = useState(8)
-const [numberAllowed , setNumberAllowed] = useState(false)
-const [charAllowed , setCharAllowed] = useState(false)
-const [password , setPassword] = useState()
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState()
 
-const passwordGenrator = useCallback(()=>{
-let pass = ''
-let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-if(numberAllowed) str+='0123456789'
-if(charAllowed) str+= '!@#$%^&*()'
-// console.log(str);
+  //useRef hook
+  const passwordRef = useRef(null)
 
-for (let i = 0; i < length; i++) {
-  let char = Math.floor(Math.random()*str.length)
- pass+= str.charAt(char)
- setPassword(pass)
-  
-}
+  const passwordGenrator = useCallback(() => {
+    let pass = ''
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    if (numberAllowed) str += '0123456789'
+    if (charAllowed) str += '!@#$%^&*()'
+    // console.log(str);
 
-} , [length , numberAllowed , charAllowed , setPassword])
-useEffect(()=>{
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length)
+      pass += str.charAt(char)
+      setPassword(pass)
 
-  passwordGenrator()
-} , [length , numberAllowed , charAllowed , setPassword])
+    }
+   
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+  const copyPassword = useCallback(() => {
+    // window.navigator.clipboard.writeText(password)
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0 , 25)
+  }, [password])
+  useEffect(() => {
+
+    passwordGenrator()
+  }, [length, numberAllowed, charAllowed, setPassword])
 
 
-function handleRange(e){
-  e.preventDefault();
-  console.log(e.target.value);
-  setLength(e.target.value)
-}
+  function handleRange(e) {
+    e.preventDefault();
+    // console.log(e.target.value);
+    setLength(e.target.value)
+  }
 
   return (
     <>
       <div className='w-full h-[100vh] bg-black text-white flex justify-center flex-col items-center gap-10'>
-              <h1 className='text-white text-4xl'>Password-Genrator</h1>
+        <h1 className='text-white text-4xl'>Password-Genrator</h1>
         <div >
 
 
-        <input 
-        type="text" 
-        className='w-[250px]
+          <input
+            type="text"
+            className='w-[250px]
          text-black'
-         value={password}    />
-        <button className='bg-blue-500 border-2 border-white mx-4 w-20'>copy</button>
+            value={password}
+            ref={passwordRef}
+            readOnly />
+          <button onClick={copyPassword} className='bg-blue-500 border-2 border-white mx-4 w-20'>copy</button>
         </div>
         <div className='flex justify-center  items-center gap-4'>
-          
-          <input className='cursor-pointer' type="range" min={8} max={25} onChange={handleRange}  />
+
+          <input className='cursor-pointer' type="range" min={8} max={25} onChange={handleRange} />
           <span>Length({length})</span>
           <input
-           type="checkbox"
+            type="checkbox"
             name="number"
-             id="number"
-             defaultChecked={numberAllowed}
-             onChange={()=>{
-              setNumberAllowed((prev)=>!prev)
-             }} />
+            id="number"
+            defaultChecked={numberAllowed}
+            onChange={() => {
+              setNumberAllowed((prev) => !prev)
+            }} />
           <label htmlFor="Number">Number</label>
-          <input 
-          type="checkbox"
-           name="symbol" 
-           id="symbol"
-           defaultChecked={charAllowed}
-           onChange={()=>{
-            setCharAllowed((prev)=>!prev) 
-           }}
-           />
+          <input
+            type="checkbox"
+            name="symbol"
+            id="symbol"
+            defaultChecked={charAllowed}
+            onChange={() => {
+              setCharAllowed((prev) => !prev)
+            }}
+          />
           <label htmlFor="">Symbol</label>
         </div>
       </div>
